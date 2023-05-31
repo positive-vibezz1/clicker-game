b59f7dff02e0d2fccd070cc2b0285d8785d82957
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2023-05-31 09:14:25 -0400

    Add files via upload
    the first functional build of the clicker game

---
+++ clicker-game.py
@@ -0,0 +1,121 @@
+from pygame import *
+from pygame import font
+from pygame import math
+import pygame
+import sys
+
+
+#display initiation aswell as fonnt initiation
+pygame.display.init()
+pygame.font.init()
+
+
+#click button
+click = 0
+clickworth = 1
+
+#upgrade button
+reducecounter = 0
+nextupgrade = 9
+
+
+#window properties
+clicker = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("clicker game")
+pygame.display.flip()
+
+
+#clicker button properties
+clickebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(375, 350, 60, 60))
+
+upgradebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(675, 130, 40, 40))
+
+
+running = True
+
+while True:
+    for event in pygame.event.get():
+        # Check for QUIT event 
+     
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+        #checks if the mouse posotion is over our click button, and if it is then it adds 1 to the total value
+        leftclick = 1
+        (x, y) = pygame.mouse.get_pos()
+
+
+        mouseposition = pygame.mouse.get_pos()
+        mousepress = MOUSEBUTTONDOWN
+
+
+        #clicker button
+        if x >= 363 and x <=433 and y >= 350 and event.type == mousepress and event.button == leftclick :
+            click += clickworth
+            print(nextupgrade)
+            #print('u clicked: ' + str(click))
+
+        
+        #upgradebutton
+        if x >= 663 and x <= 733 and y >= 100 and event.type == mousepress and event.button == leftclick:
+            if click > nextupgrade:
+                clickworth += 1
+                nextupgrade **= 2
+
+
+
+        #our number displays, how it displays aswell as color and everything which has to do with the total value
+        #click display
+        def clickbuttontoaddclick():
+            global click
+            global clickworth
+            global nextupgrade
+            #draws a black rectangle over our amount text
+            if event.type == mousepress and event.button == leftclick:
+                pygame.draw.rect(clicker, "black", pygame.Rect(70, 120, 350, 60))
+
+            font = pygame.font.SysFont("Times New Roman", 35)
+            numberdisplay = font.render('u clicked ' + str(click), True, 'pink')
+            clicker.blit(numberdisplay, (150 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
+
+            '''
+            #reduces counter until next upgrade letting u know how many more clicks 
+            def untilnextupgrade(reducecounter, clickamount):
+                if event.type == mousepress and event.button == leftclick:
+                    pygame.draw.rect(clicker, "black", pygame.Rect(440, 400, 340, 90))
+                    reducecounter -= clickamount
+
+                font = pygame.font.SysFont("Times New Roman", 35)
+                numberdisplay = font.render('next upgrade in: ' + str(reducecounter), True, 'pink')
+                clicker.blit(numberdisplay,(650 - numberdisplay.get_width() // 2, 450 - numberdisplay.get_height() // 2))
+
+                #print(reducecounter)
+            untilnextupgrade(nextupgrade, clickworth)
+            '''
+
+
+
+
+        #upgrade display
+        def clickbuttontoupgradeclick():
+            #draws a black rectangle over our upgrade text
+            if event.type == mousepress and event.button == leftclick:
+                pygame.draw.rect(clicker, "black", pygame.Rect(420, 110, 250, 100))
+
+         
+            font = pygame.font.SysFont("Times New Roman", 35)
+            numberdisplay = font.render('click worth: ' + str(clickworth), True, 'pink')
+            clicker.blit(numberdisplay,(550 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
+
+
+
+
+        clickbuttontoaddclick()
+        clickbuttontoupgradeclick()
+
+
+        
+        pygame.display.update()
+
