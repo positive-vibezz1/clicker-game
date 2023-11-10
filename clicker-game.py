8ae0274da3261c67d137942f1cc2345b6116ae5f
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2023-11-10 14:53:15 -0500

    newest code base
    cleaned up the code, and hopefully made it easier to upgrade and remake the clicker game

--- clicker-game.py
+++ clicker-game.py
@@ -16,7 +16,6 @@ click = 0
 clickworth = 1
 
 #upgrade button
-nextupgradetwo = 18
 nextupgrade = 9
 
 #reduce counter logic
@@ -24,37 +23,65 @@ reducecounter = 0
 bottomout = 0
 
 #window properties
-clicker = pygame.display.set_mode((800, 600))
+size = 800, 600
+clicker = pygame.display.set_mode((size))
+surface = pygame.Surface(size)
 pygame.display.set_caption("clicker game")
 pygame.display.flip()
 
-'''
-clickicon = pygame.image.load('sponge bob.png')
-pygame.image.save(clickicon, 'sponge bob.png')
-pygame.display.set_icon(clickicon)
-'''
+#our number displays, how it displays aswell as color and everything which has to do with the total value
+#click display
+def clickbuttontoaddclick():
 
-#clicker button properties
-clickebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(325, 350, 60, 60))
+    font = pygame.font.SysFont("Times New Roman", 35)
+    numberdisplay = font.render('u clicked: ' + str(click), True, 'pink')
+    clicker.blit(numberdisplay, (150 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
 
-upgradebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(675, 130, 40, 40))
+#reduces counter until next upgrade letting u know how many more clicks 
+def untilnextupgrade(countdown):
 
-#upgradebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(675, 360, 40, 40))
+    if countdown >= click - 1:
+        countdown -= click - 1
 
+    else:
+        countdown = 0
+
+
+
+    font = pygame.font.SysFont("Times New Roman", 35)
+    numberdisplay = font.render('next upgrade in: ' + str(countdown), True, 'pink')
+    clicker.blit(numberdisplay,(584 - numberdisplay.get_width() // 2, 250 - numberdisplay.get_height() // 2))
+
+    return countdown
+
+    #print(reducecounter)
+    #print(click)
+            
+#upgrade display
+def clickbuttontoupgradeclick():
+       
+    font = pygame.font.SysFont("Times New Roman", 35)
+    numberdisplay = font.render('click worth: ' + str(clickworth), True, 'pink')
+    clicker.blit(numberdisplay,(550 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
+
+
+clock = pygame.time.Clock()
 
 running = True
-
-
-
-
 while True:
+    clicker.fill((0, 0, 0))
+
     for event in pygame.event.get():
         # Check for QUIT event 
-     
+
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+         #clicker button properties
+        clickebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(325, 350, 60, 60))
+
+        upgradebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(675, 130, 40, 40))
 
         #checks if the mouse posotion is over our click button, and if it is then it adds 1 to the total value
         leftclick = 1
@@ -78,82 +105,8 @@ while True:
                 clickworth += 1
                 nextupgrade *= 3
 
-        '''
-        #upgradebuttontwo
-        if x >= 663 and x <= 733 and y >= 352 and y <= 402 and event.type == mousepress and event.button == leftclick:
-            print('i clicked this button')
-            if click > nextupgradetwo:
-                clickworth += 1
-                nextupgrade *= 4
-        '''
-                
-
-        #our number displays, how it displays aswell as color and everything which has to do with the total value
-        #click display
-        def clickbuttontoaddclick():
-            global click
-            global clickworth
-            global nextupgrade
-            #draws a black rectangle over our amount text
-            if event.type == mousepress and event.button == leftclick:
-                pygame.draw.rect(clicker, "black", pygame.Rect(70, 120, 350, 60))
-
-            font = pygame.font.SysFont("Times New Roman", 35)
-            numberdisplay = font.render('u clicked: ' + str(click), True, 'pink')
-            clicker.blit(numberdisplay, (150 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
-
-
-        #reduces counter until next upgrade letting u know how many more clicks 
-        def untilnextupgrade(countdown):
-            if event.type == mousepress and event.button == leftclick:
-                pygame.draw.rect(clicker, "black", pygame.Rect(440, 200, 320, 90))
-
-            if countdown >= click - 1:
-                countdown -= click - 1
-
-            else:
-                countdown = 0
-
-
-
-            font = pygame.font.SysFont("Times New Roman", 35)
-            numberdisplay = font.render('next upgrade in: ' + str(countdown), True, 'pink')
-            clicker.blit(numberdisplay,(584 - numberdisplay.get_width() // 2, 250 - numberdisplay.get_height() // 2))
-
-            return countdown
-
-            #print(reducecounter)
-            #print(click)
-            
-
-        #upgrade display
-        def clickbuttontoupgradeclick():
-
-            #draws a black rectangle over our upgrade text
-            if event.type == mousepress and event.button == leftclick:
-                pygame.draw.rect(clicker, "black", pygame.Rect(420, 110, 250, 100))
-       
-            font = pygame.font.SysFont("Times New Roman", 35)
-            numberdisplay = font.render('click worth: ' + str(clickworth), True, 'pink')
-            clicker.blit(numberdisplay,(550 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
-
-        '''
-        def clickbuttontoupgradeclicktwo():
-            print("hello world")
-            if event.type == mousepress and event.button == leftclick:
-                pygame.draw.rect(clicker, "black", pygame.Rect(440, 200, 320, 90))
-
-            font = pygame.font.SysFont("Times New Roman", 35)
-            numberdisplay = font.render('click worth: ' + str(clickworth), True, 'pink')
-            clicker.blit(numberdisplay,(550 - numberdisplay.get_width() // 2, 250 - numberdisplay.get_height() // 2))
-        '''
-
-
-
-    untilnextupgrade(nextupgrade)
-    clickbuttontoaddclick()
-    clickbuttontoupgradeclick()
-
-        
-    pygame.display.update()
-
+        clock.tick(60)   
+        untilnextupgrade(nextupgrade)
+        clickbuttontoaddclick()
+        clickbuttontoupgradeclick()
+        pygame.display.flip()
