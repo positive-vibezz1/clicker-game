3a5e3c7af3fe15f81cfb55bb98ea6ac95871afbe
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2023-11-20 18:31:49 -0500

    clicker-game.py
    the newest code base

--- clicker-game.py
+++ clicker-game.py
@@ -5,23 +5,54 @@ import pygame
 import os
 import sys
 
+font_path = "Montserrat-Medium.ttf"
+
+if getattr(sys, 'frozen', False):
+    # If the script is run as a bundled executable
+    base_path = sys._MEIPASS
+else:
+    
+    # If the script is run as a regular Python script
+    base_path = os.path.abspath(os.path.dirname(__file__))
+#where our font is stored locally? idk?
+font_path = os.path.join(base_path, font_path)
+
 
 #display initiation aswell as fonnt initiation
 pygame.display.init()
 pygame.font.init()
+font1 = pygame.font.Font("Montserrat-Medium.ttf", 36)
+font2 = pygame.font.Font("Montserrat-Medium.ttf", 28)
+
+#colours
+button_color_pink = (255, 192, 203)
+button_color_black = (0, 0, 0)
+button_color_white = (255, 255, 255)
+button_color_red = (255, 0, 0)
 
 
-#click button
-click = 0
-clickworth = 1
+class text:
+    def __init__(self, position, text, font):
+        self.position = position
+        self.text = text
+        self.font = font
 
-#upgrade button
-nextupgrade = 9
+    def textrender(self, surface):
+        text_surface = self.font.render(self.text, True, (255, 255, 255))
+        surface.blit(text_surface, self.position)
 
-#reduce counter logic
-reducecounter = 0
-bottomout = 0
+class Button:
+    def __init__(self, B_position, B_size, color, the_screen):
+        self.B_position = B_position
+        self.B_size = B_size
+        self.color = color
+        self.the_screen = the_screen
+        self.rect = Rect(self.B_position[0], self.B_position[1], self.B_size[0], self.B_size[1])
 
+    def button_render(self):
+        pygame.draw.rect(self.the_screen, self.color, self.rect) 
+
+       
 #window properties
 size = 800, 600
 clicker = pygame.display.set_mode((size))
@@ -29,45 +60,21 @@ surface = pygame.Surface(size)
 pygame.display.set_caption("clicker game")
 pygame.display.flip()
 
-#our number displays, how it displays aswell as color and everything which has to do with the total value
-#click display
-def clickbuttontoaddclick():
 
-    font = pygame.font.SysFont("Times New Roman", 35)
-    numberdisplay = font.render('u clicked: ' + str(click), True, 'pink')
-    clicker.blit(numberdisplay, (150 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
+# Create the text object
+score_display = text((10, 10), "you clicked: ", font1)
+until_next_upgrade = text((500, 200), "next upgrade: ", font2)
 
-#reduces counter until next upgrade letting u know how many more clicks 
-def untilnextupgrade(countdown):
+# create a button object
+main_button = Button((100, 100), (50, 50), button_color_pink, clicker)
 
-    if countdown >= click - 1:
-        countdown -= click - 1
-
-    else:
-        countdown = 0
-
-
-
-    font = pygame.font.SysFont("Times New Roman", 35)
-    numberdisplay = font.render('next upgrade in: ' + str(countdown), True, 'pink')
-    clicker.blit(numberdisplay,(584 - numberdisplay.get_width() // 2, 250 - numberdisplay.get_height() // 2))
-
-    return countdown
-
-    #print(reducecounter)
-    #print(click)
-            
-#upgrade display
-def clickbuttontoupgradeclick():
-       
-    font = pygame.font.SysFont("Times New Roman", 35)
-    numberdisplay = font.render('click worth: ' + str(clickworth), True, 'pink')
-    clicker.blit(numberdisplay,(550 - numberdisplay.get_width() // 2, 150 - numberdisplay.get_height() // 2))
-
+# creates the rects
+#doesthiswork = button((200, 200), (60, 60))
 
 clock = pygame.time.Clock()
 
 running = True
+
 while True:
     clicker.fill((0, 0, 0))
 
@@ -78,35 +85,14 @@ while True:
             pygame.quit()
             sys.exit()
 
-         #clicker button properties
-        clickebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(325, 350, 60, 60))
-
-        upgradebutton = pygame.draw.rect(clicker, "pink", pygame.Rect(675, 130, 40, 40))
-
-        #checks if the mouse posotion is over our click button, and if it is then it adds 1 to the total value
-        leftclick = 1
-        (x, y) = pygame.mouse.get_pos()
-
-
-        mouseposition = pygame.mouse.get_pos()
-        mousepress = MOUSEBUTTONDOWN
-
-
-        #clicker button
-        if x >= 325 and x <=383 and y >= 350 and event.type == mousepress and event.button == leftclick:
-            click += clickworth
-            #print(nextupgrade)
-            #print('u clicked: ' + str(click))
-
+        # Draw the text on the screen
+        score_display.textrender(clicker)
+        until_next_upgrade.textrender(clicker)
         
-        #upgradebuttonone
-        if x >= 663 and x <= 733 and y >= 100 and event.type == mousepress and event.button == leftclick:
-            if click > nextupgrade:
-                clickworth += 1
-                nextupgrade *= 3
+        #draw the button on the screen
+        main_button.button_render()
+        
 
-        clock.tick(60)   
-        untilnextupgrade(nextupgrade)
-        clickbuttontoaddclick()
-        clickbuttontoupgradeclick()
+        clock.tick(60 )    
         pygame.display.flip()
+
