9e7aeb2f2eb4aeb6b99fa27133efb640974358d5
positive_vibezz <134086715+positive-vibezz1@users.noreply.github.com>
2023-12-10 14:16:04 -0500

    Add files via upload

--- clicker-game.py
+++ clicker-game.py
@@ -6,7 +6,7 @@ import os
 import sys
 from text import text
 from button import Button
-
+from recursion import Recursion
 
 font_path = "Montserrat-Medium.ttf"
 
@@ -22,8 +22,12 @@ font_path = os.path.join(base_path, font_path)
 
 
 #click logic
-upgrade_logic_1 = 3*4-6
+upgrade_logic_1 = 3 ** 2
+upgrade_logic_2 = Recursion(2,2)
+
 next_upgrade_1 = 10
+next_upgrade_2 = 100
+
 main_counter = 0
 click_worth = 1
 
@@ -50,11 +54,15 @@ pygame.display.flip()
 
 # Create the text object
 score_display = text((10, 10), "you clicked: ", font1) 
-until_next_upgrade = text((500, 200), "next upgrade in: " + str(next_upgrade_1), font2)
+until_next_upgrade_1 = text((450, 100), "next upgrade at: " + str(next_upgrade_1), font2)
+until_next_upgrade_2 = text((450, 250), "next upgrade at: " + str(next_upgrade_2), font2)
 
 # create a button object
 main_button = Button((100, 100), (50, 50), button_color_pink, clicker)
-upgrade_button_1 = Button((600, 300), (50, 50), button_color_red, clicker)
+upgrade_button_1 = Button((600, 170), (50, 50), button_color_red, clicker)
+upgrade_button_2 = Button((600, 300), (50, 50), button_color_pink, clicker)
+
+
 
 clock = pygame.time.Clock()
 running = True
@@ -67,31 +75,51 @@ while True:
             pygame.quit()
             sys.exit()
 
-
         (mousex, mousey) = pygame.mouse.get_pos()
         mousepress = event.type == MOUSEBUTTONDOWN
         
         if main_button.is_clicked((mousex, mousey), mousepress):
             main_counter += click_worth
             score_display.text = "you clicked: " + str(main_counter)
-
-            
-        if upgrade_button_1.is_clicked((mousex, mousey), mousepress):
+            print("main_counter:", main_counter)
+            print(f"your click worth is {click_worth}")
+           
+        if upgrade_button_1.is_clicked((mousex, mousey), mousepress):            
             if main_counter >= next_upgrade_1:
-                next_upgrade_1 += upgrade_logic_1      
+                next_upgrade_1 += upgrade_logic_1
                 click_worth += 1
-            until_next_upgrade.text = "next upgrade in: " + str(next_upgrade_1)
+            until_next_upgrade_1.text = "next upgrade at: " + str(next_upgrade_1)
+            print(f"next_upgrade_1: {next_upgrade_1}")
+            print(f"your click worth: {click_worth}")
+            print("upgrade_logic_1e:", next_upgrade_1)
+       
+        if upgrade_button_2.is_clicked((mousex, mousey), mousepress):
+            power_result = upgrade_logic_2.recursive_power(next_upgrade_2, upgrade_logic_2.exponent) 
+            if main_counter >= next_upgrade_2:
+                next_upgrade_2 += power_result  
+                click_worth += 2
+                print(f"Power Result: {power_result}")
+                until_next_upgrade_2.text = "next upgrade at: " + str(next_upgrade_2)
+            print(f"next_upgrade_1: {next_upgrade_2}")
+            print(f"your click worth: {click_worth}")
+            print("upgrade_logic:", next_upgrade_2)
 
 
-
-        # Draw the text on the screen
-        score_display.textrender(clicker)
-        until_next_upgrade.textrender(clicker)
+    # Draw the text on the screen
+    score_display.textrender(clicker)
+    until_next_upgrade_1.textrender(clicker)
+    until_next_upgrade_2.textrender(clicker)
         
-        #draw the button on the screen
-        main_button.button_render()
-        upgrade_button_1.button_render()
         
+    #draw the button on the screen
+    main_button.button_render()
+    upgrade_button_1.button_render()
+    upgrade_button_2.button_render()
     
-        clock.tick(60)    
-        pygame.display.flip()
+    #click amount display
+    click_amoun_display = text((10, 400), "click amount: " + str(click_worth), font2)
+    click_amoun_display.textrender(clicker)
+    
+    
+    clock.tick(60)    
+    pygame.display.flip()
